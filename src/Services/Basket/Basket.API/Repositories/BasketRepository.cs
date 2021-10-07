470098025895162ba110915fd803124035749564
Welder Marcos <welder.silva@techmindit.com.br>
2021-10-07 08:15:15 -0300

    basket service + portainer service

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,39 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    internal class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+
+        public async Task DeleteBasket(string username)
+        {
+            await _redisCache.RemoveAsync(username);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string username)
+        {
+            var basket = await _redisCache.GetStringAsync(username);
+            if (string.IsNullOrEmpty(basket))
+                return null;
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart)
+        {
+            await _redisCache.SetStringAsync(shoppingCart.Username, JsonConvert.SerializeObject(shoppingCart));
+
+            return await GetBasket(shoppingCart.Username);
+        }
+    }
+}
